b6a192764538a01d7a3a6e9d9979c91175efb49a
Vinicius <vinicius.luiz.9256@gmail.com>
2022-07-28 13:00:31 -0300

    show-scroll refactored from function to class

---
+++ js/modules/show-scroll.js
@@ -0,0 +1,39 @@
+export default class ShowScroll{
+  constructor(list) {
+    this.sections = document.querySelectorAll(list);
+    this.windowAHalf = window.innerHeight * 0.6;
+    this.checkDistance = this.checkDistance.bind(this);
+  }
+
+  // Pega a distância de cada item em relação ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      console.log(offset)
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowAHalf),
+      };
+    });
+  }
+
+  // verifica a distância em cada objeto em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if(window.pageYOffset > item.offset) 
+        item.element.classList.add('ativo');
+      else if(item.element.classList.contains('ativo'))
+        item.element.classList.remove('ativo');
+    });
+  }
+
+  // Inicia as funções
+  init() {
+    if(this.sections.length){
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-import SmoothScroll from "./modules/smooth-scroll.js";
 import Accordion from "./modules/accordion.js";
-import TabNav from "./modules/nav-tab.js";
-import Modal from "./modules/modal.js";
-import ToolTip from "./modules/tooltip.js";
 import createNumbersAnimals from "./modules/create-numbers-animals.js";
+import Modal from "./modules/modal.js";
+import SmoothScroll from "./modules/smooth-scroll.js";
+import TabNav from "./modules/nav-tab.js";
+import ToolTip from "./modules/tooltip.js";
+import ShowScroll from "./modules/show-scroll.js";
 
 import initDropDownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initDateObject from "./modules/date-object.js";
 
-import initAnimationScroll from "./modules/animacao-scroll.js";
 
 const scrollSuave = new SmoothScroll('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -17,8 +17,8 @@ scrollSuave.init();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
-const tabnav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
-tabnav.init();
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+tabNav.init();
 
 const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
 modal.init();
@@ -28,7 +28,9 @@ tooltip.init();
 
 createNumbersAnimals('../../animals-api.json' ,'.numeros-grid');
 
+const showScroll = new ShowScroll('[data-anime="scroll"]');
+showScroll.init();
+
 initDropDownMenu();
 initMenuMobile();
 initDateObject();
-initAnimationScroll();
